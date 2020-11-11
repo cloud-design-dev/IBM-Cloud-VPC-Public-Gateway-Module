529677dad7042f775f19fa1f36df054966833836
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 12:14:32 -0600

    fixed zone call
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource ibm_is_public_gateway gateway {
-  count          = length(var.zones)
+  count          = length(var.zone)
   name           = "${var.name}-gateway-zone-${count.index + 1}"
   vpc            = var.vpc_id
-  zone           = var.zones[count.index]
+  zone           = var.zone[count.index]
   resource_group = data.ibm_resource_group.group.id
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable vpc_id {}
 variable name {}
-variable zones {}
+variable zone {}
 variable resource_group {}
