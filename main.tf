0624e27f4438b81b3cdc2a99b29005c0915080d0
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 12:24:59 -0600

    Taking count out of modules
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource ibm_is_public_gateway gateway {
-  count          = length(var.zone)
-  name           = "${var.name}-gateway-zone-${count.index + 1}"
+  name           = "${var.name}-${var.zone}"-gateway"
   vpc            = var.vpc_id
-  zone           = var.zone[count.index]
+  zone           = var.zone
   resource_group = data.ibm_resource_group.group.id
 }
