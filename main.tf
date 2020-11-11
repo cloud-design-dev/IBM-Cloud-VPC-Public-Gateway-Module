e0f0a80a518fdb0bdab0bd7e993d36b37cee5b83
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 12:31:58 -0600

    Fixed syntax error in main.tf
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource ibm_is_public_gateway gateway {
-  name           = "${var.name}-${var.zone}"-gateway"
+  name           = "${var.name}-${var.zone}-gateway"
   vpc            = var.vpc_id
   zone           = var.zone
   resource_group = data.ibm_resource_group.group.id
