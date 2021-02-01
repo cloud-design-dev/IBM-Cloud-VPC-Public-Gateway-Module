bcb73f564d87fc244b73882b599fc519e6853334
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-02-01 10:44:43 -0600

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,7 +1,10 @@
 terraform {
+  required_version = "<= 0.14.0"
   required_providers {
+
     ibm = {
-      source = "IBM-Cloud/ibm"
+      source  = "IBM-Cloud/ibm"
+      version = "<= 1.20.1"
     }
   }
 }
