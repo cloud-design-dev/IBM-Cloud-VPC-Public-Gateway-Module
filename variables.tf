1ccb185ada50cd8845ba8de73bddce766bb6619f
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-02-02 09:30:01 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,14 @@ variable vpc_id {}
 variable name {}
 variable zone {}
 variable resource_group_id {}
-variable floating_ip {
-  description = "Floating IP to assign to Public Gateway."
+variable floating_ip_id {
+  description = "ID of the floating IP to assign to Public Gateway."
+  type        = string
+  default     = ""
+}
+
+variable floating_ip_address {
+  description = "Address of the floating IP to assign to Public Gateway."
   type        = string
   default     = ""
 }
