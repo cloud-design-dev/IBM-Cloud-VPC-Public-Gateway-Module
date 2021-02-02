acc97a2d16143465ce9c0d291a37a5e589af5a10
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-02-02 09:28:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,12 @@ resource ibm_is_public_gateway gateway {
   name           = var.name
   vpc            = var.vpc_id
   zone           = var.zone
-  resource_group = var.resource_group
+  resource_group = var.resource_group_id
+  floating_ip    = local.floating_ip
+  tags           = var.tags
+  
+}
+
+locals {
+  floating_ip  = var.floating_ip_id != "" ? var.floating_ip = {id = floating_ip_id} : var.floating_ip = {address = floating_ip_address}
 }
