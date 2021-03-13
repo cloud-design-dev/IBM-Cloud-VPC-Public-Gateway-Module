c3a9af9768e1a677b78462d56c82a40092a86973
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-03-13 15:22:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource ibm_is_public_gateway gateway {
   name           = var.name
   vpc            = var.vpc_id
   zone           = var.zone
-  resource_group = var.resource_group_id
+  resource_group = var.resource_group
   tags           = var.tags
   
 }
